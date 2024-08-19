7fd436dc5b29a445645e76fbf7d9c95f4305e8ae
Yuchan Kim <yuchankim0906@gmail.com>
2024-08-19 16:53:35 +0900

    SpringBoot3 Controller JSP

---
+++ src/main/java/com/javaex/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+	
+	//Field
+	
+	//Constructor
+	
+	//Methods (GS)
+	
+	//Methods (General)
+	
+	@RequestMapping (value = "/hello", method = {RequestMethod.GET, RequestMethod.POST})
+	public String hello() {
+		
+		System.out.println("HelloController.hello()");
+		
+		return "/WEB-INF/views/hello.jsp";
+	}
+}
---
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -0,0 +1,19 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PhonebookController {
+	
+	
+	@RequestMapping(value ="/writeform", method = {RequestMethod.GET, RequestMethod.POST} )
+	public String writeForm() {
+		System.out.println("PhonebookController.writeForm()");
+		
+		
+		
+		return "/WEB-INF/views/writeForm.jsp";
+	}
+}
